269f44686f1d84ff60295297ad9adec9f30b4dd4
unknown <realdanielalexander@gmail.com>
2020-11-26 12:10:10 +0700

    add hardcode catalog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,25 @@
 <template>
-
-     <b-navbar>
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
-                >
-            </b-navbar-item>
-        </template>
-        <template slot="start">
-            <b-navbar-item href="/">
-                Home
-            </b-navbar-item>
-            <b-navbar-item href="#">
-               Items
-            </b-navbar-item>
-            
-        </template>
-     </b-navbar>
-
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
+        />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="/"> Home </b-navbar-item>
+      <b-navbar-item href="#"> Items </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
 
 <script>
 export default {
-    name:"Home"
-}
+  name: "Home",
+};
 </script>
 
 <style>
-
 </style>
